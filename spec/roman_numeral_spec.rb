6c3464a5135e8dfc7989de6e4380edc29262d6d2
Phil Borel <phil_borel@yahoo.com>
2015-09-17 15:20:41 -0400

    add second test for roman_numerals arabic to roman

--- spec/roman_numeral_spec.rb
+++ spec/roman_numeral_spec.rb
@@ -5,5 +5,9 @@ describe 'converting an Arabic number to a Roman numeral' do
     it 'converts 1 to I' do
       expect(convert_to_roman(1)).to eq "I"
     end
+
+    it 'converts 4 to IIII' do
+      expect(convert_to_roman(4)).to eq "IIII"
+    end
   end
 end
